1c891690dc2951b5a6943d8e17e20dbbb2be7c8c
RickDev <98334211+r1ckd3v@users.noreply.github.com>
2025-05-23 16:49:58 -0400

    just outsourced actions into a separate file cart-actions.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 
-import { sendCartData } from "./store/cart-slice";
+import { sendCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
